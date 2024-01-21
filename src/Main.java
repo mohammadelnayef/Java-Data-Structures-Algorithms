ae95634c0f0c742d05b09c89b6f917702493b5a4
Mohammad Elnayef <mohammadelnayef@gmail.com>
2024-01-21 19:21:49 +0200

    Implemented the is balanced expression method using the stack data structure

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,10 @@ import java.util.Stack;
 public class Main {
     public static void main(String[] args) {
 
-        Stack<Integer> stack = new Stack<>();
-        stack.push(10);
-        stack.push(20);
-        stack.push(30);
-
         var mohStack = new MohStack();
-        var result = mohStack.reverseString("sony");
+        var result = mohStack.isBalancedExpression("{hello world}");
         System.out.println(result);
 
-//        System.out.println(stack);
-//
-//        var top = stack.pop();
-//        System.out.println(top);
-//
-//        System.out.println(stack);
     }
 }
 
--- src/MohStack.java
+++ src/MohStack.java
@@ -27,4 +27,49 @@ public class MohStack {
         // After the stack is empty, and we have the complete reversed string in the buffer, we convert it to a String and return it.
         return reversed.toString();
     }
+
+    public boolean isBalancedExpression(String input)
+    {
+        //We create a stack
+        Stack<Character> openingTags = new Stack<>();
+
+        // Then we loop trough each character of the input String
+        for(char character : input.toCharArray()){
+
+            //If the current character in the loop is an opening tag, we push it into the stack.
+            if(character == '(' || character == '[' || character == '<' || character == '{'){
+                openingTags.push(character);
+            }
+
+            //If the current character in the loop is a closing tag, we do some checks:
+            if(character == ')' || character == ']' || character == '>' || character == '}'){
+
+                // We first check if the stack contains an opening bracket, if not, it means the expression is not balanced,
+                // so we exit with the result set to false.
+                if(openingTags.empty()){
+                    return false;
+                }
+
+                // If the stack is not empty, we pop the last element from the top of the stack, and we compare it with the
+                // current character, we do this in order to check that the current tag has its corresponding closing tag
+                var top = openingTags.pop();
+
+                if(
+                    (character == ')' && top != '(' ) ||
+                    (character == ']' && top != '[' ) ||
+                    (character == '>' && top != '<' ) ||
+                    (character == '}' && top != '{' )
+                ){
+                    // In case any of these checks is truthy, it means that the opening bracket doesn't have the correct closing bracket
+                    // So we exit the function and return false.
+                    return false;
+                }
+
+            }
+        }
+
+        // Lastly, we return the result of the Stack is empty method. If the stack is empty, the expression is balanced.
+        return openingTags.empty();
+    }
+
 }
