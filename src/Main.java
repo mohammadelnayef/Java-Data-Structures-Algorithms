2653f3607fa1253679bd9d5d66651015099193e0
Mohammad Elnayef <mohammadelnayef@gmail.com>
2024-04-01 14:44:58 +0300

    Implemented solution for find first non repeating char

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,8 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-        MohHash mohMap = new MohHash();
-        var firstNonRepeatableChar = mohMap.findFirstNonRepeatedCharacter("a green apple");
-        System.out.println(firstNonRepeatableChar);
-
-        var firstNonRepeatableChar2 = mohMap.findFirstNonRepeatedCharacter("aaaa");
-        System.out.println(firstNonRepeatableChar2);
-
-        var firstNonRepeatableChar3 = mohMap.findFirstNonRepeatedCharacter("");
-        System.out.println(firstNonRepeatableChar3);
+        CharFinder finder = new CharFinder();
+        finder.findFirstNonRepeatingChar("a green apple");
 
     }
 }
