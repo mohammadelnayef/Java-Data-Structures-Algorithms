b4d057e95dfad305a9475bb9ba039508045bbc9d
Mohammad Elnayef <mohammadelnayef@gmail.com>
2024-04-01 15:00:28 +0300

    Implemented find first repeated character using set

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-        CharFinder finder = new CharFinder();
-        finder.findFirstNonRepeatingChar("a green apple");
-
+        MohSetFinder set = new MohSetFinder();
+        var result = set.findFirstRepeatingChar("a green apple");
+        System.out.println(result);
     }
 }
 
---
+++ src/MohSetFinder.java
@@ -0,0 +1,22 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class MohSetFinder {
+
+    public char findFirstRepeatingChar(String str){
+
+        Set<Character> set  = new HashSet();
+        var chars = str.toCharArray();
+
+        for(var ch : chars){
+
+            if(set.contains(ch)){
+                return ch;
+            }
+            else {
+                set.add(ch);
+            }
+        }
+        return Character.MIN_VALUE;
+    }
+}
