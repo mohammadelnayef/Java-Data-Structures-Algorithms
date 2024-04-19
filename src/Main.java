f2512ccf3498e89808ae551da74cdd9ee680bfd6
Mohammad Elnayef <mohammadelnayef@gmail.com>
2024-04-19 11:21:14 +0300

    Created efficient Hash Table class

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,14 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-        MohHashClass myHash = new MohHashClass();
-        myHash.put(1, "Mohammad");
-        myHash.put(2, "Tom");
-        myHash.put(3, "Jim");
-        myHash.put(4, "Mike");
-        myHash.put(5, "Rex");
-        myHash.put(6, "Tim");
-        myHash.put(100, "Ramy");
-
-        myHash.remove(100);
-
-        myHash.displayItems();
+        HashTable table = new HashTable();
+        table.put(6,"A");
+        table.put(8, "B");
+        table.put(11, "C");
+        table.put(6, "A+");
+        table.remove(60);
+        System.out.println(table.get(10));
     }
 }
+
 
