ab0c46e1e1f5fc2700585ab50ba3df155b7adccb
Mohammad Elnayef <mohammadelnayef@gmail.com>
2024-08-17 16:44:16 +0300

    Implemented Trie findWords method aka autocomplete

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,11 @@ import Trie.Trie;
 public class Main {
     public static void main(String[] args) {
         Trie trie = new Trie();
-        trie.insert("canada");
-        trie.traverse();
+        trie.insert("card");
+        trie.insert("care");
+        trie.insert("careful");
+        var words = trie.findWords("car");
+        System.out.println(words);
 
     }
 }
