fa20cc4bd238ad6e4ca1486bfa59fb13d38212c1
Mohammad Elnayef <mohammadelnayef@gmail.com>
2024-08-17 15:28:01 +0300

    Implemented Trie delete method

--- src/Trie/Trie.java
+++ src/Trie/Trie.java
@@ -35,6 +35,16 @@ public class Trie {
             return children.values().toArray(new Node[0]);
         }
 
+        public boolean hasChildren()
+        {
+            return !children.isEmpty();
+        }
+
+        public void removeChild(char ch)
+        {
+            children.remove(ch);
+        }
+
         @Override
         public String toString() {
             return "value= " + value;
@@ -83,6 +93,35 @@ public class Trie {
 
         for(var child : root.getChildren()){
             traverse(child);
+        }
+    }
+
+    public void remove(String word)
+    {
+        if(word == null){
+            return;
+        }
+        remove(root,word, 0);
+    }
+
+    private void remove(Node root,String word, int index)
+    {
+        if(index == word.length()){
+            root.isEndOfWord = false;
+            return;
+        }
+
+        var ch = word.charAt(index);
+        var child = root.getChild(ch);
+
+        if(child == null){
+            return;
+        }
+
+        remove(child, word, index + 1);
+
+        if(!child.hasChildren() && !child.isEndOfWord){
+            root.removeChild(ch);
         }
     }
 }
