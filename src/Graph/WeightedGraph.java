eff212b91d41e64ad529dbd97f686c978a3e9b48
Mohammad Elnayef <mohammadelnayef@gmail.com>
2024-08-30 12:14:57 +0300

    Implemented get shorted path method

--- src/Graph/WeightedGraph.java
+++ src/Graph/WeightedGraph.java
@@ -69,6 +69,115 @@ public class WeightedGraph {
         toNode.addEdge(fromNode, weight);
     }
 
+    private class NodeEntry {
+        private Node node;
+        private int priority;
+
+        public NodeEntry(Node node, int priority) {
+            this.node = node;
+            this.priority = priority;
+        }
+    }
+
+    public Path getShortestPath(String from, String to)
+    {
+        var fromNode = nodes.get(from);
+        if(fromNode == null){
+            throw new IllegalArgumentException();
+        }
+        var toNode = nodes.get(to);
+        if(toNode == null){
+            throw  new IllegalArgumentException();
+        }
+
+        Map<Node,Integer> distances = new HashMap<>();
+
+        for(var node : nodes.values()){
+            distances.put(node, Integer.MAX_VALUE);
+        }
+        distances.replace(fromNode, 0);
+
+        Map<Node, Node> previousNodes = new HashMap<>();
+
+        Set<Node> visited = new HashSet<>();
+
+        PriorityQueue<NodeEntry> queue = new PriorityQueue<>(
+                Comparator.comparingInt( ne -> ne.priority )
+        );
+
+        queue.add(new NodeEntry(fromNode, 0));
+
+        while(!queue.isEmpty()){
+            var current = queue.remove().node;
+            visited.add(current);
+
+            for(var edge : current.getEdges()){
+                if(visited.contains(edge.to)){
+                    continue;
+                }
+
+                var newDistance = distances.get(current) + edge.weight;
+
+                if(newDistance < distances.get(edge.to)){
+                    distances.replace(edge.to, newDistance);
+                    previousNodes.put(edge.to, current);
+                    queue.add(new NodeEntry(edge.to, newDistance));
+                }
+            }
+        }
+
+        return buildPath(previousNodes, toNode);
+    }
+
+    private Path buildPath(Map<Node, Node> previousNodes , Node toNode)
+    {
+        Stack<Node> stack = new Stack<>();
+        stack.push(toNode);
+        var previous = previousNodes.get(toNode);
+
+        while(previous != null ){
+            stack.push(previous);
+            previous = previousNodes.get(previous);
+        }
+
+        var path = new Path();
+
+        while(!stack.isEmpty()){
+            path.add(stack.pop().label);
+        }
+
+        return path;
+    }
+
+    public boolean hasCycle()
+    {
+        Set<Node> visited = new HashSet<>();
+        for(var node : nodes.values()){
+            if(!visited.contains(node) && hasCycle(node, null ,visited)){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean hasCycle(Node node, Node parent, Set<Node> visited)
+    {
+        visited.add(node);
+
+        for(var edge : node.getEdges()){
+            if(edge.to == parent){
+                continue;
+            }
+
+            if(visited.contains(edge.to) || hasCycle(edge.to, node, visited)){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     public void print()
     {
         for(var node : nodes.values()){
